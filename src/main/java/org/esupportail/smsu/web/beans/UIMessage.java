b1248d9de5d2e59ad5173e33d7f43ca3f3c59136
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2013-12-06 12:06:46 +0100

    WIP adapt/normalize UIMessage to web-service needs

--- src/main/java/org/esupportail/smsu/web/beans/UIMessage.java
+++ src/main/java/org/esupportail/smsu/web/beans/UIMessage.java
@@ -1,176 +1,91 @@
 package org.esupportail.smsu.web.beans;
 
-import java.util.Set;
+import java.util.List;
 
-import org.esupportail.commons.services.logging.Logger;
-import org.esupportail.commons.services.logging.LoggerImpl;
-import org.esupportail.smsu.dao.beans.Message;
-import org.esupportail.smsu.dao.beans.Person;
+public class UIMessage {
 
-/**
- * @author xphp8691
- *
- */
-public class UIMessage extends Message  {
-
-	private static final long serialVersionUID = 1L;
-	
-	private Integer nbRecipients;
+	public Integer id;
+	public Integer nbRecipients;
 
 	/**
 	 * senderName.
 	 */
-	private String senderName;
+	public String senderName;
 	
 	/**
 	 * group names.
 	 */
-	private String groupSenderName;
-	private String groupRecipientName;
+	public String groupSenderName;
+	public String groupRecipientName;
 	
-	private String stateMessage;
+	public String stateMessage;
 	
-	private String stateMail;
-	private String supervisorsText;
+	public String stateMail;
+	public List<String> supervisors;
 
-	/**
-	 * Log4j logger.
-	 */
-	@SuppressWarnings("unused")
-	private final Logger logger = new LoggerImpl(getClass());
 	
-	/**
-	 * constructor.
-	 */
-	public UIMessage() {
-		super();
-	}
-	
-	/**
-	 * @param stateMessage 
-	 * @param stateMail  
-	 * @param senderName
-	 * @param groupSenderName
-	 * @param groupRecipientName
-	 * @param message
-	 */
-	public UIMessage(final String stateMessage, final String stateMail, final String senderName, 
-			final String groupSenderName, final String groupRecipientName, final Message message) {
-		super(message);
-		this.stateMessage = stateMessage;
-		this.stateMail = stateMail;
-		this.senderName = senderName;
-		this.groupSenderName = groupSenderName;
-		this.groupRecipientName = groupRecipientName;
-
-		this.nbRecipients = message.getRecipients().size();
-		this.supervisorsText = computeSupervisorsText(message.getSupervisors());
-	}
-	
-	private String computeSupervisorsText(Set<Person> supervisors) {
-		if (supervisors == null) return null;
-		String t = null;
-		for (Person p : supervisors)
-			t = (t == null ? "" : t + ", ") + p.getLogin();
-		return t;
+	public Integer getId() {
+		return id;
 	}
 
-	/**
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((senderName == null) ? 0 : senderName.hashCode());
-		result = prime * result;
-		return result;
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
-
-
-	/* *
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}	
-		if (obj == null) {
-			return false;
-		}	
-		if (getClass() != obj.getClass()) {
-			return false;
-		}	
-		UIMessage other = (UIMessage) obj;
-		if (senderName == null) {
-			if (other.senderName != null) {
-				return false;
-		    }
-		} else if (!senderName.equals(other.senderName)) {
-			return false;
-		  }
-		return true;
-	}
-
-	public int getNbRecipients() {
+	public Integer getNbRecipients() {
 		return nbRecipients;
 	}
 
-	/**
-	 * @param senderName
-	 */
-	public void setSenderName(final String senderName) {
-		this.senderName = senderName;
+	public void setNbRecipients(Integer nbRecipients) {
+		this.nbRecipients = nbRecipients;
 	}
 
-	/**
-	 * @return senderName
-	 */
 	public String getSenderName() {
 		return senderName;
+	}
+
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
 	}
 
 	public String getGroupSenderName() {
 		return groupSenderName;
 	}
 
+	public void setGroupSenderName(String groupSenderName) {
+		this.groupSenderName = groupSenderName;
+	}
+
 	public String getGroupRecipientName() {
 		return groupRecipientName;
 	}
 
-	/**
-	 * @param stateMessage the stateMessage to set
-	 */
-	public void setStateMessage(final String stateMessage) {
-		this.stateMessage = stateMessage;
+	public void setGroupRecipientName(String groupRecipientName) {
+		this.groupRecipientName = groupRecipientName;
 	}
 
-	/**
-	 * @return the stateMessage
-	 */
 	public String getStateMessage() {
 		return stateMessage;
 	}
 
-	/**
-	 * @param stateMail the stateMail to set
-	 */
-	public void setStateMail(final String stateMail) {
-		this.stateMail = stateMail;
+	public void setStateMessage(String stateMessage) {
+		this.stateMessage = stateMessage;
 	}
 
-	/**
-	 * @return the stateMail
-	 */
 	public String getStateMail() {
 		return stateMail;
 	}
 
-	public String getSupervisorsText() {
-		return supervisorsText;
+	public void setStateMail(String stateMail) {
+		this.stateMail = stateMail;
+	}
+
+	public List<String> getSupervisors() {
+		return supervisors;
+	}
+
+	public void setSupervisors(List<String> supervisors) {
+		this.supervisors = supervisors;
 	}
 
 }
