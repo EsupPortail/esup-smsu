431ed634ef68292937bea2e7ee5203c3823dea0f
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2013-12-06 10:59:37 +0100

    WIP add jaxrs exception mappers

---
+++ src/main/java/org/esupportail/smsu/web/controllers/exceptionmappers/CreateMessageExceptionMapper.java
@@ -0,0 +1,17 @@
+package org.esupportail.smsu.web.controllers.exceptionmappers;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.esupportail.smsu.exceptions.CreateMessageException;
+
+
+@Provider
+public class CreateMessageExceptionMapper implements
+        ExceptionMapper<CreateMessageException> {
+    @Override
+    public Response toResponse(CreateMessageException ex) {
+        return Helper.jsonErrorResponse(400, ex.toString());
+    }
+}
