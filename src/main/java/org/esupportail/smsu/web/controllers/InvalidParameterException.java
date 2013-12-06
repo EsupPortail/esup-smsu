01f4229ead70cc18ddfb93c345fa50f55e6e8caa
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2013-12-06 11:01:11 +0100

    WIP add basic jaxrs exception for validation

---
+++ src/main/java/org/esupportail/smsu/web/controllers/InvalidParameterException.java
@@ -0,0 +1,9 @@
+package org.esupportail.smsu.web.controllers;
+
+@SuppressWarnings("serial")
+public class InvalidParameterException extends java.lang.IllegalArgumentException {
+    
+    public InvalidParameterException(String err) {
+        super(err);
+    }
+}
