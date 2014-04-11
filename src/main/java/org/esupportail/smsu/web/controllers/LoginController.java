EsupPortail/esup-smsu-api-admin

WIP add LoginController for new web-service

src/org/esupportail/smsuapiadmin/web/controllers/LoginController.java
@@ -1,0 +1,35 @@
+package org.esupportail.smsuapiadmin.web.controllers;
+
+import java.io.IOException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.esupportail.smsuapiadmin.domain.DomainService;
+import org.esupportail.smsuapiadmin.dto.beans.UIUser;
+
+@Path("/login")
+public class LoginController {
+	
+    @Autowired
+    private DomainService domainService;
+
+    @GET
+    public Response get(@Context HttpServletRequest request) throws IOException {
+	UIUser user = domainService.getUserByLogin(request.getRemoteUser());
+	String jsUser = new ObjectMapper().writeValueAsString(user);
+	String callback = request.getParameter("callback");
+	String type = callback == null ? "text/html" : "application/x-javascript";
+	String js = 
+	    callback == null ?
+	    "Login success, please wait...\n<script>\n console.log('sending postMessage');\n (parent.postMessage ? parent : parent.document).postMessage('loggedUser=' + JSON.stringify(" + jsUser + "), '*');\n</script>" :
+	    callback + "(" + jsUser + ")";
+	
+        return Response.status(Response.Status.OK).type(type).entity(js).build();
+    }
+}

EsupPortail/esup-smsu

src/main/java/org/esupportail/smsu/web/controllers/LoginController.java
@@ -1,0 +1,35 @@
+package org.esupportail.smsu.web.controllers;
+
+import java.io.IOException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.esupportail.smsu.domain.DomainService;
+import org.esupportail.smsu.dto.beans.UIUser;
+
+@Path("/login")
+public class LoginController {
+	
+    @Autowired
+    private DomainService domainService;
+
+    @GET
+    public Response get(@Context HttpServletRequest request) throws IOException {
+	UIUser user = domainService.getUserByLogin(request.getRemoteUser());
+	String jsUser = new ObjectMapper().writeValueAsString(user);
+	String callback = request.getParameter("callback");
+	String type = callback == null ? "text/html" : "application/x-javascript";
+	String js = 
+	    callback == null ?
+	    "Login success, please wait...\n<script>\n console.log('sending postMessage');\n (parent.postMessage ? parent : parent.document).postMessage('loggedUser=' + JSON.stringify(" + jsUser + "), '*');\n</script>" :
+	    callback + "(" + jsUser + ")";
+	
+        return Response.status(Response.Status.OK).type(type).entity(js).build();
+    }
+}