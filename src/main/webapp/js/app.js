fe2586b1f3de14cc150ea57d7fd9b9631b0935fd
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2014-06-05 12:56:26 +0200

    code cleanup: remove unused var

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,7 @@
 (function () {
 "use strict";
 
-var app = angular.module('myApp', ['ngGrid', 'ngRoute',
+angular.module('myApp', ['ngGrid', 'ngRoute',
 				   'pascalprecht.translate', 'ui.bootstrap']);
 
 })();
