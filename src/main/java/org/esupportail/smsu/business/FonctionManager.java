4c0b92db8b6107e260049e10270bf51a58c90346
Vincent Bonamy <vbonamy@gmail.com>
2015-11-27 17:55:53 +0100

    Remove FonctionName filter for obsolete functions ...

--- src/main/java/org/esupportail/smsu/business/FonctionManager.java
+++ src/main/java/org/esupportail/smsu/business/FonctionManager.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.esupportail.smsu.dao.DaoService;
 import org.esupportail.smsu.dao.beans.Fonction;
-import org.esupportail.smsu.domain.beans.fonction.FonctionName;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -27,17 +26,9 @@ public class FonctionManager {
 	public List<String> getAllFonctions() {
 		List<String> result = new ArrayList<String>();
 		for (Fonction fct : daoService.getFonctions()) {
-			if (toFonctionName(fct.getName()) != null) // filter obsolete Fonctions (eg: FCTN_APPROBATION_ENVOI)
-				result.add(fct.getName());
+			result.add(fct.getName());
 		}
 		return result;
-	}
-
-	static FonctionName toFonctionName(String name) {
-		for (FonctionName n : FonctionName.values()) {
-			if (name.equals(n.name())) return n;
-		}
-		return null;
 	}
 
 }
