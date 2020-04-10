d22658f35c350a5ab170c0447236e0bc11c36a2b
Vincent Bonamy <vbonamy@gmail.com>
2020-04-10 11:55:24 +0200

    Fix unique constraint on RCP_PHONE

--- src/main/resources/database/migrate_3.2.sql
+++ src/main/resources/database/migrate_3.2.sql
@@ -1,4 +1,5 @@
 -- remove unique on RCP_PHONE
 alter table recipient modify RCP_PHONE varchar(255) not null;
+drop index RCP_PHONE on recipient;
 -- add new index on both phone&login
 create index RCP_PHONE_LOGIN on recipient (RCP_PHONE, RCP_LOGIN);
