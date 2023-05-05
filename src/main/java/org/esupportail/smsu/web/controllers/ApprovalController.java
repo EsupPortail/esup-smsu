266f9f02d48f9e96bdaa0ae9d4c1df0e72f86664
Florian Nari <Florian.Nari@univ-paris1.fr>
2023-05-05 11:33:28 +0200

    correction of a mistake in the commit 732a387

--- src/main/java/org/esupportail/smsu/web/controllers/ApprovalController.java
+++ src/main/java/org/esupportail/smsu/web/controllers/ApprovalController.java
@@ -1,22 +1,24 @@
 package org.esupportail.smsu.web.controllers;
 
+import java.util.Collection;
 import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import org.apache.commons.lang3.EnumUtils;
+import org.apache.log4j.Logger;
 import org.esupportail.smsu.business.ApprovalManager;
 import org.esupportail.smsu.domain.DomainService;
 import org.esupportail.smsu.domain.beans.User;
 import org.esupportail.smsu.domain.beans.message.MessageStatus;
 import org.esupportail.smsu.exceptions.CreateMessageException;
 import org.esupportail.smsu.web.beans.UIMessage;
-import org.apache.log4j.Logger;
-import javax.inject.Inject;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,15 +36,19 @@ public class ApprovalController {
     	return approvalManager.getApprovalUIMessages(request);
     }
     
+	private static final Collection<MessageStatus> CANCEL_OR_IN_PROGRESS = List.of(MessageStatus.CANCEL, MessageStatus.IN_PROGRESS);
+
 	@RequestMapping(method = RequestMethod.PUT, value = "/{id:\\d+}")
 	public void modify(@PathVariable("id") int id, HttpServletRequest request, @RequestBody UIMessage msg) throws CreateMessageException {
-		String status = msg.stateMessage;
-		if (status.equals("CANCEL") && status.equals("IN_PROGRESS")) {
-			throw new InvalidParameterException("unknown status " + status);
+		String statusStr = msg.getStateMessage();
+		MessageStatus status = EnumUtils.getEnum(MessageStatus.class, statusStr);
+		
+		if((status == null) || (!CANCEL_OR_IN_PROGRESS.contains(status))) {
+			throw new InvalidParameterException("unknown status " + statusStr);
 		}
+		
 		User currentUser = domainService.getUser(request);
-		logger.info("" + currentUser + " " + (status.equals("CANCEL") ? "cancel" : "approve") + " message " + id);
-		approvalManager.cancelOrApproveMessage(id, currentUser, MessageStatus.valueOf(status), request);
+		logger.info(currentUser + " " + (MessageStatus.CANCEL.equals(status) ? "cancel" : "approve") + " message " + id);
+		approvalManager.cancelOrApproveMessage(id, currentUser, status, request);
 	}
-
 }
